f1e66c8d08cc327439485c2b70c94b9d11778159
aminafarah <amishkamohamed563@gmail.com>
2023-11-09 11:06:42 +0300

    initialized create react app

---
+++ dog/src/App.js
@@ -0,0 +1,22 @@
+
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+
+
+
+function App(){
+    return(
+    <div>
+        <Navbar/>
+        <Home/>
+        <Favorites/>
+    </div>
+
+        
+    
+);}
+
+
+export default App;
